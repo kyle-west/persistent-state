8f6c031fc210e78a0a7dfd0a86b37e7c0ec32c94
Kyle West <kyle@familysearch.org>
2019-08-13 09:54:13 -0600

    [WC] add a test element for better support

---
+++ demo/custom-webcomponent.js
@@ -0,0 +1,28 @@
+(() => {
+  const name = 'this-is-a-custom-wc'
+
+  class CustomInputComponent extends HTMLElement {
+    connectedCallback () {
+      this.attachShadow({mode: 'open'}); // this will hide the input from <persistent-state>
+      this.shadowRoot.innerHTML = `
+        <h6>This is a custom Web Component with custom events to make it hard to use with persistent state</h6>
+      `
+      this.textInput = document.createElement('input');
+      this.shadowRoot.appendChild(this.textInput);
+
+      this.textInput.addEventListener('input', (e) => {
+        this.dispatchEvent(new CustomEvent(`${name}::input`, { 
+          bubbles: true, 
+          composed: true, 
+          detail: { internalInputValue: e.currentTarget.value }
+        }))
+      })
+    }
+  }
+  
+  if ('customElements' in window) {
+    customElements.define(name, CustomInputComponent);
+  } else {
+    document.registerElement(name, {prototype: Object.create(CustomInputComponent.prototype)});
+  }
+})()
